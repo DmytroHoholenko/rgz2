d2aa740cf0f6278351c217c968835d850b33a4ea
Dmytro Hoholenko <gogol1994.dg@gmail.com>
2023-06-17 14:20:42 +0300

    nice

--- rgz.cpp
+++ rgz.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <cmath>
-#include <cstring>
+#include <string.h>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
@@ -27,25 +24,18 @@ void dec2bin(fstream& decFile, fstream& binFile) {
     }
 }
 
-bool areEqual(char* str1, char* str2) {
-    for (int i = 0; i < 8; i++)
-        if (str1[i] != str2[i])
-            return false;
-    return true;
-}
-
 void bin2let(fstream& binFile, fstream& bin2letFile, char* matrix[], int row, int col) {
     char bin1[9];
     char bin2[9];
     char let;
     for (int m = 1; m < row; m++) {
         for (int n = 1; n < col; n++) {
-            binFile.read(bin1, 8);
+            binFile.get(bin1, 9);
             bin2letFile.seekg(0);
             while (!bin2letFile.eof()) {
                 bin2letFile >> bin2;
                 bin2letFile >> let;
-                if (areEqual(bin1, bin2)) {
+                if (strcmp(bin1, bin2) == 0) {
                     matrix[m][n] = let;
                     break;
                 }
@@ -57,94 +47,39 @@ void bin2let(fstream& binFile, fstream& bin2letFile, char* matrix[], int row, in
 void addKeyWords(char* matrix[], const char* w1, const char* w2, int row, int col) {
     matrix[0][0] = ' ';
     for (int i = 1; i < row; i++) {
-        matrix[0][i] = w1[i - 1];
-    }
-    for (int i = 1; i < col; i++) {
         matrix[i][0] = w2[i - 1];
     }
-}
-
-
-/*void sortRows(char* matrix[], int row, int col)
-{
-    for (int j = 1; j < col; j++)
-    {
-        for (int i = 0; i < row; i++) {
-            sort(matrix[i] + 1, matrix[i] + col);
-        }
+    for (int i = 1; i < col; i++) {
+        matrix[0][i] = w1[i - 1];
     }
 }
 
-void sortColumns(char* matrix[], int row, int col) 
-{
-    for (int j = 0; j < col; j++) {
-        char* column = new char [row];
-        for (int i = 0; i < row; i++) {
-            column[i] = matrix[i][j];
+void sortRows(char* matrix[], int row, int col) {
+    for (int i = 1; i < col; i++) {
+        int max = i;
+        for (int j = i + 1; j < row; j++) {
+            if (matrix[max][0] > matrix[j][0])
+                max = j;
         }
-        sort(column, column + row);
-        for (int i = 1; i < row; i++) {
-            matrix[i][j] = column[i];
-        }
-    }
-}*/
-
-/*void sortrow(char* matrix[], int row, int col)
-{
-    char** columnArray = new char* [row];
-    for (int i = 0; i < row; i++) {
-        columnArray[i] = new char[col];
-    }
-    for (int i = 0; i < row; i++) 
-    {
-        for (int j = 1; j < row; j++)
-        {
-            columnArray[i][j] = matrix[i][j];
-        }
-    }
-    sort(columnArray, columnArray + row);
-    for (int i = 0; i < row; i++) 
-    {
-        for (int j = 1; j < row; j++)
-        {
-            matrix[i][j] = columnArray[i][j];
-        }
-    }
-    delete[] columnArray;
-}*/
-
-void sortRow(char* matrix[], int row, int col) {
-    for (size_t colIndex = 1; colIndex < 5; colIndex++) {
-        for (size_t i = 1; i < row; i++) {
-            size_t min = i;
-
-            for (size_t j = i + 1; j < row; j++) {
-                if (matrix[j][colIndex] < matrix[min][colIndex]) {
-                    min = j;
-                }
-            }
-
-            if (min != i) {
-                char* tmp = matrix[i];  
-                matrix[i] = matrix[min];
-                matrix[min] = tmp;
+        if (max != i) {
+            for (int j = 0; j < row; j++) {
+                int tmp = matrix[max][j];
+                matrix[max][j] = matrix[i][j];
+                matrix[i][j] = tmp;
             }
         }
     }
 }
 
 void sortCols(char** matrix, int rows, int cols) {
-   
     char* tempRow = new char[cols];
     std::copy(matrix[0], matrix[0] + cols, tempRow);
 
     std::sort(tempRow, tempRow + cols);
 
-
     char** tempMatrix = new char* [rows];
-    for (int i = 0; i < rows; i++) {
+    for (int i = 0; i < rows; i++)
         tempMatrix[i] = new char[cols];
-    }
 
     for (int j = 0; j < cols; j++) {
         char letter = tempRow[j];
@@ -157,27 +92,44 @@ void sortCols(char** matrix, int rows, int cols) {
             }
         }
 
-        for (int i = 0; i < rows; i++) {
+        for (int i = 0; i < rows; i++)
             tempMatrix[i][j] = matrix[i][originalIndex];
-        }
     }
 
-    for (int i = 0; i < rows; i++) {
+    for (int i = 0; i < rows; i++)
         std::copy(tempMatrix[i], tempMatrix[i] + cols, matrix[i]);
-    }
 
-    for (int i = 0; i < rows; i++) {
+    for (int i = 0; i < rows; i++)
         delete[] tempMatrix[i];
-    }
     delete[] tempMatrix;
     delete[] tempRow;
 }
-
 
 int main() {
     fstream decFile;
     fstream binFile;
     fstream bin2letFile;
+
+    // === get num ===============
+    int numNum, number;
+    decFile.open("dec.txt", std::fstream::out);
+    cout << "Set number(double) of numbers: ";
+    do {
+        cin >> numNum;
+        if (numNum % 2 != 0) {
+            cout << "Set other number!\n";
+        }
+    } while (numNum % 2 != 0);
+
+    cout << "Set digits: ";
+    for (int i = 0; i < numNum; i++) {
+        cin >> number;
+        decFile << number;
+        if (i + 1 != numNum)
+            decFile << ' ';
+    }
+    decFile.close();
+    // ===========================
 
     // === dec2bin ===============
     decFile.open("dec.txt", std::fstream::in);
@@ -193,6 +145,21 @@ int main() {
     binFile.close();
     // ===========================
 
+    // === get key words =========
+    char w1[10] = "";
+    char w2[10] = "";
+    int row, col;
+    cout << "Set key words. Condition: len(w1)*len(w2) == number of numbers\n";
+    do {
+        cin >> w1;
+        cin >> w2;
+        row = strlen(w2);
+        col = strlen(w1);
+        if (row * col != numNum)
+            cout << "Set other words!\n";
+    } while (row * col != numNum);
+    // ===========================
+
     // === bin2let ===============
     binFile.open("bin.txt", std::fstream::in);
     bin2letFile.open("bin2let.txt", std::fstream::in);
@@ -201,42 +168,22 @@ int main() {
         return 0;
     }
 
-    int row = 5;
-    int col = 5;
-
+    row++;
+    col++;
     char** matrix = new char* [row];
     for (int i = 0; i < row; i++) {
         matrix[i] = new char[col];
     }
 
-    bin2let(binFile, bin2letFile, matrix, 5, 5);
-    addKeyWords(matrix, "save", "fish", 5, 5);
+    addKeyWords(matrix, w1, w2, row, col);
+    bin2let(binFile, bin2letFile, matrix, row, col);
 
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++)
-            cout << matrix[i][j] << ' ';
-        cout << endl;
-    }
+    binFile.close();
+    bin2letFile.close();
+    // ===========================
 
-    cout << endl;
-
-    
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++)
-            cout << matrix[i][j] << ' ';
-        cout << endl;
-    }
-    cout << endl;
-    
-    sortRow(matrix, row, col);
-
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++)
-            cout << matrix[i][j] << ' ';
-        cout << endl;
-    }
-    cout << endl;
-
+    // === sort ==================
+    sortRows(matrix, row, col);
     sortCols(matrix, row, col);
 
     for (int i = 0; i < row; i++) {
@@ -245,17 +192,13 @@ int main() {
         cout << endl;
     }
     cout << endl;
-
-    binFile.close();
-    bin2letFile.close();
     // ===========================
 
     // === del arr ===============
-    for (int i = 0; i < col; i++)
+    for (int i = 0; i < row; i++)
         delete[] matrix[i];
     delete[] matrix;
     // ===========================
 
     return 0;
 }
-
